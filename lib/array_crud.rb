9ed2ffff20b2d946251237f8d71ac892d2d84d41
Jill Short <jillian.b.short@gmail.com>
2020-07-10 23:13:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,9 +25,9 @@ def remove_element_from_start_of_array(array)
   .shift
 end
 
-def retrieve_element_from_index(array, 2)
+def retrieve_element_from_index(array, index_number)
   ["wow", "I", "am", "really", "learning", "arrays!"]
-  
+  retrieve_element_from_index[2]
 end
 
 def retrieve_first_element_from_array(array)
